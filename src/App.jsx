bdbd6ddfc3f8a4c80c71b5a1fa2181ce672f7416
brenoMatias <matiasbreno19@gmail.com>
2022-02-17 09:49:12 -0300

    menu settled

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import Intro from "./components/intro/Intro";
+import Menu from "./components/menu/Menu";
+
 import "./app.scss"
 import { useState } from "react";
 
 function App() {
-  // const [menuOpen,setMenuOpen] = useState(false)
+  const [menuOpen,setMenuOpen] = useState(false)
   return (
     <div className="app">
      <div className="sections">
        <Intro/>
+       <Menu menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
+    
      </div>
     </div>
   );
