b414c13dce5e92692046e809d873f6a8f49fb251
brenoMatias <matiasbreno19@gmail.com>
2022-02-21 14:16:07 -0300

    settling burger menu functionality

--- src/App.jsx
+++ src/App.jsx
@@ -3,21 +3,26 @@ import Menu from "./components/menu/Menu";
 import Topbar from "./components/topbar/Topbar"
 import Portfolio from "./components/portfolio/Porftolio";
 import "./app.scss";
-import Works from "./components/works/Works"
+import Works from "./components/works/Works";
+import Contact from "./components/contact/Contact";
 
 
 import "./app.scss"
 import { useState } from "react";
 
 function App() {
+  const [menuOpen,setMenuOpen] = useState(false) // HOOKS
+  // set burger Menu
   return (
     <div className="app">
-      <Topbar/>
+     <Topbar menuOpen={menuOpen} setMenuOpen={setMenuOpen}/> 
+     <Menu menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
 
      <div className="sections">
        <Intro />
       <Portfolio/>
       <Works />
+      <Contact />
 
     
      </div>
